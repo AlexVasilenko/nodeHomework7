d19fab95dbd2c89aa37d5f2a57328939837887d2
Alexander Korzhykov <fractalawareness@gmail.com>
2017-01-18 23:04:00 +0200

    lesson3 cloud service refactoring

---
+++ lesson3/cloud/controllers/upload.js
@@ -0,0 +1,58 @@
+var multer  = require('multer');
+var fs = require('fs');
+var config = require('config');
+
+var uploadMiddleware = multer({
+  dest: config.uploadDestination,
+  fileFilter: fileFilter
+});
+
+var FILES_LIST_PATH = config.filesListPath;
+var FILERECORDSPLITTER = config.filesRecordSplitter;
+var filesList = [];
+
+try {
+  filesList = fs.readFileSync(FILES_LIST_PATH, 'utf8').split(FILERECORDSPLITTER);
+  console.log('FileList loaded at start');
+} catch (err) {
+  console.log('No fileList found at start');
+}
+
+function fileFilter(req, file, cb) {
+  var filePath = req.query.filePath;
+  if (filesList.includes(filePath)) {
+    cb(null, false);
+  } else {
+    saveToFileList(filePath, function(err) {
+      cb(err, true);
+    });
+  }
+}
+
+function validateRequest() {
+  return function(req, res, next) {
+    var filePath = req.query.filePath;
+    if (!filePath) {
+      var err = new Error('Validation error: filePath parameter is missing');
+      err.code = 400;
+      return next(err);
+    }
+    next();
+  }
+}
+
+function saveToFileList(filePath, cb) {
+  try {
+    filesList.push(filePath)
+    var fileRecord = filePath + FILERECORDSPLITTER;
+    fs.appendFileSync(FILES_LIST_PATH, fileRecord);
+  } catch (err) {
+    return cb(err);
+  }
+  console.log('File saved to list', filePath);
+  return cb();
+}
+module.exports = {
+  parse: uploadMiddleware,
+  validate: validateRequest
+};
--- lesson3/cloud/routes/index.js
+++ lesson3/cloud/routes/index.js
@@ -1,30 +1,12 @@
-var multer  = require('multer');
 var config = require('../config');//TO-DO: switch to config module
 var basic = require('basic-auth');
-var fs = require('fs');
 var router = require('express').Router();
-
-var uploadMiddleware = multer({
-  dest: config.uploadDestination,
-  fileFilter: fileFilter
-});
-
-var FILES_LIST_PATH = config.filesListPath;
-var FILERECORDSPLITTER = config.filesRecordSplitter;
-var filesList = [];
-
-try {
-  filesList = fs.readFileSync(FILES_LIST_PATH, 'utf8').split(FILERECORDSPLITTER);
-  console.log('FileList loaded at start');
-} catch (err) {
-  console.log('No fileList found at start');
-}
+var uploadCtrl = require('../controllers/upload');
 
 router.post('/upload',
   basicAuth(),
-  validateRequest(),
-  uploadMiddleware.single('syncfile'),
-  saveToFileList(),
+  uploadCtrl.validate(),
+  uploadCtrl.parse.single('syncfile'),
   function(req, res) {
     console.log(req.file ? 'Uploaded' : 'Duplicate', req.query.filePath);
     res.status(200).end();
@@ -44,42 +26,5 @@ function basicAuth() {
   };
 }
 
-function fileFilter(req, file, cb) {
-  var filePath = req.query.filePath;
-  if (filesList.includes(filePath)) {
-    console.log('Duplicate', filePath);
-    cb(null, false);
-  } else {
-    console.log('New file', filePath);
-    cb(null, true);
-  }
-}
-
-function validateRequest() {
-  return function(req, res, next) {
-    var filePath = req.query.filePath;
-    if (!filePath) {
-      var err = new Error('Validation error: filePath parameter is missing');
-      err.code = 400;
-      return next(err);
-    }
-    next();
-  }
-}
-
-function saveToFileList() {
-  return function(req, res, next) {
-    var filePath = req.query.filePath;
-    try {
-      filesList.push(filePath)
-      var fileRecord = filePath + FILERECORDSPLITTER;
-      fs.appendFileSync(FILES_LIST_PATH, fileRecord);
-    } catch (err) {
-      return next(err);
-    }
-    console.log('File saved to list', filePath);
-    return next();
-  }
-}
 
 module.exports = router;
