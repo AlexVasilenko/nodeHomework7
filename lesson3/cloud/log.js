33aed2ba561bf079b269e8d1321f95f7fbc98853
Alexander Korzhykov <fractalawareness@gmail.com>
2017-01-19 00:29:02 +0200

    lesson3 colorize logs in cloud service

--- lesson3/cloud/log.js
+++ lesson3/cloud/log.js
@@ -2,7 +2,7 @@ var winston = require('winston');
 
 var logger = new winston.Logger({
     transports: [
-      new winston.transports.Console({'timestamp':true}),
+      new winston.transports.Console({ 'timestamp': true, colorize: true }),
       new winston.transports.File({ filename: './logs/cloud.log' })
     ]
 });
