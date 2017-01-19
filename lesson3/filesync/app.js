16353be7281dbd3ef6a682bf6addaa7dea052e72
Alexander Korzhykov <fractalawareness@gmail.com>
2017-01-19 13:42:29 +0200

    lesson3 promisify filesync cli tool upload controller

--- lesson3/filesync/app.js
+++ lesson3/filesync/app.js
@@ -19,28 +19,29 @@ var userArgs = process.argv.slice(2);
 command
   .arguments('<file>')
   .option('-u, --username <username>', 'Enter username:')
-  .action(function(file) {//TO-DO: promisify
-    promptly.prompt('Enter password:', { validator: validator, silent: true }, function (err, password) {
-      if (err) {
-        return exitWithError(err);
-      }
-      cloud.upload(file, command.username, password, function(err, res) {
-        if (err) {
-          return exitWithError(err);
-        }
-        console.log(chalk.green('File synced'), file);
-        process.exit(0);
-      });
-    });
-  })
-  // //promisified + ES6
-  // .action(file => promptly.prompt('Enter password:', { validator: validator, silent: true })
-  //   .then(password => cloud.upload(file, command.username, password))//TO-DO: promisify
-  //   .then(() => {
-  //       console.log('File synced', file);
+  // .action(function(file) {
+  //   promptly.prompt('Enter password:', { validator: validator, silent: true }, function (err, password) {
+  //     if (err) {
+  //       return exitWithError(err);
+  //     }
+  //     cloud.upload(file, command.username, password, function(err, res) {
+  //       if (err) {
+  //         return exitWithError(err);
+  //       }
+  //       console.log(chalk.green('File synced'), file);
   //       process.exit(0);
-  //     })
-  // )
+  //     });
+  //   });
+  // })
+  //promisified + ES6
+  .action(file => promptly.prompt('Enter password:', { validator: validator, silent: true })
+    .then(password => cloud.upload(file, command.username, password))//TO-DO: promisify
+    .then(() => {
+        console.log('File synced', file);
+        process.exit(0);
+      })
+    .catch(exitWithError)
+  )
   .parse(process.argv);
 
 function exitWithError(err) {
--- lesson3/filesync/cloudClient.js
+++ lesson3/filesync/cloudClient.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
 var agent = require('superagent');
 var ProgressBar = require('ascii-progress');
@@ -10,33 +11,31 @@ module.exports = {
 };
 
 function postFileToCloud(filePath, username, password, cb) {
-  fs.stat(filePath, function(err, stats){//TO-DO: bluebird promisify
-    if (err) {
-      return cb(err);
-    }
-    console.log('Trying to sync file', filePath, 'with size', stats.size);
-    var bar = new ProgressBar({
-        schema: 'uploading [:bar] :percent',
-        total: 100,
-        width: 20
+  return fs.statAsync(filePath)
+    .then(stats => {
+      console.log('Trying to sync file', filePath, 'with size', stats.size);
+      var bar = new ProgressBar({
+          schema: 'uploading [:bar] :percent',
+          total: 100,
+          width: 20
+      });
+      var fileStream = fs.createReadStream(filePath);
+      var uploadUrl = generateUploadUrl(filePath);
+      return agent
+        .post(uploadUrl)
+        .auth(username, password)
+        .type('form')
+        .on('progress', function(e) {
+          var percentDone = Math.floor((e.loaded / e.total) * 100);
+          bar.tick(percentDone);
+        })
+        .attach('syncfile', fileStream)
+        .set('Accept', 'application/json');
     });
-    var fileStream = fs.createReadStream(filePath);
-    var uploadUrl = generateUploadUrl(filePath);
-    agent
-      .post(uploadUrl)
-      .auth(username, password)
-      .type('form')
-      .on('progress', function(e) {
-        var percentDone = Math.floor((e.loaded / e.total) * 100);
-        bar.tick(percentDone);
-      })
-      .attach('syncfile', fileStream)
-      .set('Accept', 'application/json')
-      .end(cb);
-  });
 }
 
 function generateUploadUrl(filePath) {
   var filePathQuery = encodeURI(path.resolve(filePath));
+  console.log('filePathQuery generated', filePathQuery);
   return CLOUD_URL + '?filePath=' + filePathQuery;
 }
