308655572283acfe660fde636c19f6fb228d5305
Alexander Korzhykov <fractalawareness@gmail.com>
2017-02-02 18:21:19 +0200

    lesson7 README update

--- lesson7/authN/controllers/db.js
+++ lesson7/authN/controllers/db.js
@@ -6,5 +6,6 @@ module.exports = {
 };
 
 function checkCredentials(username, password) {
+  // let's imagine that we go to oracle DB here, so this is a 'heavy' operation
   return username === VALID_USER && password === VALID_PASS;
 }
